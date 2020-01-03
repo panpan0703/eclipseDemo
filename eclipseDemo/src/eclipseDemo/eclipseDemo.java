19cc3eb7bf1bd2228b0448743f31cb185e0ad287
panpan <1362431720@qq.com>
2020-01-03 18:48:12 +0800

    冲突完了

--- eclipseDemo/src/eclipseDemo/eclipseDemo.java
+++ eclipseDemo/src/eclipseDemo/eclipseDemo.java
@@ -5,10 +5,7 @@ public class eclipseDemo {
 	//我从github.com来
 	public static void main(String[] args) {
 		System.out.println(123);
-<<<<<<< HEAD
 		System.out.println(789);
-=======
 		System.out.println(456);
->>>>>>> branch 'master' of git@github.com:panpan0703/eclipseDemo.git
 	}
 }
